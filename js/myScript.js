2bf149012022b4a9e7410ec8e66df6b78b1d03d3
Ellen Marin <marin.ellen@gmail.com>
2017-08-03 00:19:27 +0000

    3rd push all files 8/2

--- js/myScript.js
+++ js/myScript.js
@@ -1,41 +1,43 @@
 /*global $*/
 
-
-
-
-
-
-
-
-
-
+//height quiz on games page
 $(document).ready(function(){
-            $('#ansHgt').click(function() {
-                
-           var owtcHeight, usrFeet, usrFtoI, usrInches, usrHeight, usrOWTC;
-            owtcHeight = 21312;
-            usrFeet = document.getElementById('uhf').value;
-            usrFtoI = usrFeet * 12;
-            usrInches = document.getElementById('uhi').value;
-            usrHeight = Number(usrFtoI) + Number(usrInches);
-            usrOWTC = owtcHeight / usrHeight;
-            $('#heightAnswer').hide(); //just in case
-            $('#heightAnswer').html ("We would need " + usrOWTC +  " of you!");
-            $('#heightAnswer').fadeIn(2000);
-           
-            });
+    $('#ansHgt').click(function() {
+        var owtcHeight, usrFeet, usrFtoI, usrInches, usrHeight, usrOWTC;
+        owtcHeight = 21312;
+        usrFeet = document.getElementById('uhf').value;
+        usrFtoI = usrFeet * 12;
+        usrInches = document.getElementById('uhi').value;
+        usrHeight = Number(usrFtoI) + Number(usrInches);
+        usrOWTC = owtcHeight / usrHeight;
+            if (usrOWTC == Infinity ) {
+                $('#heightAnswer').hide(); //just in case
+                $('#heightAnswer').html ("Please enter your height first and then click Calculate.");
+                $('#heightAnswer').css('color', '#C40233');
+                $('#heightAnswer').fadeIn(2000);  
+            } else {
+                $('#heightAnswer').hide(); //just in case
+                $('#heightAnswer').html ("We would need " + (+usrOWTC.toFixed(2)) +  " of you!");
+                $('#heightAnswer').css('color', '#11206f');
+                $('#heightAnswer').fadeIn(2000);
+                }
+    });
 });
             
             
-
-
-
-
-
-function getAnswer() {
-                if (document.getElementById("opt3").checked) {
-                document.getElementById('movieAnswer').innerHTML = ("You are correct! Armageddon featured the Chrysler Building.");
-                } else {
-                document.getElementById('movieAnswer').innerHTML = ("Sorry, the correct answer is Armageddon.");
-                }    
-}
+//movie quiz on games page
+$(document).ready(function(){
+    $('#checkAnswer').click(function() {
+        if ($('#opt3').prop('checked') == true) {
+            $('#movieAnswer').hide(); //just in case
+            $('#movieAnswer').html ("You are correct! Armageddon featured the Chrysler Building.");
+            $('#movieAnswer').css('color', '#01761e');
+            $('#movieAnswer').fadeIn(2000);
+        } else {
+            $('#movieAnswer').hide(); //just in case
+            $('#movieAnswer').html ("Sorry, the correct answer is Armageddon.");
+            $('#movieAnswer').css('color', '#C40233');
+            $('#movieAnswer').fadeIn(2000);
+        }
+    }); 
+});
